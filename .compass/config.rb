ce9e858a2b2c7e2b8d8eb9aa998b912d64ea5605
Harald Haesler <github@werkkreis.org>
2014-06-11 20:59:22 +0200

    svg dir

--- .compass/config.rb
+++ .compass/config.rb
@@ -4,6 +4,7 @@ project_type = :stand_alone
 css_dir = "assets/css"
 sass_dir = "assets/_sass"
 fonts_dir = "assets/fonts"
+svg_dir = "assets/svg"
 images_dir = "images"
 javascripts_dir = "assets/js"
 output_style = :expanded
