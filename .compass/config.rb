2bca5a05956e628a3c5728ca9df41a8d4ba67560
Harald Haesler <github@werkkreis.org>
2014-06-11 19:46:51 +0200

    hoppla

--- .compass/config.rb
+++ .compass/config.rb
@@ -1,2 +1,9 @@
-require 'jekyll-compass'
-project_type = :jekyll
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+project_type = :stand_alone
+css_dir = "stylesheets"
+sass_dir = "assets/_sass"
+images_dir = "images"
+output_style = :compact
+relative_assets = false
+line_comments = false
