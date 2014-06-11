9de816cb97e8574e2bb005b467c54bdb188436ab
Harald Haesler <github@werkkreis.org>
2014-06-11 20:29:54 +0200

    alerts

--- .compass/config.rb
+++ .compass/config.rb
@@ -3,7 +3,9 @@ require 'bootstrap-sass'
 project_type = :stand_alone
 css_dir = "assets/css"
 sass_dir = "assets/_sass"
+fonts_dir = "assets/fonts"
 images_dir = "images"
-output_style = :compact
-relative_assets = false
-line_comments = false
+javascripts_dir = "assets/js"
+output_style = :expanded
+relative_assets = true
+line_comments = true
