79b33a1b847243a93034c88011bc354d37a5f011
Harald Haesler <github@werkkreis.org>
2014-07-08 19:12:37 +0200

    sassy

--- .compass/config.rb
+++ .compass/config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 project_type = :stand_alone
 css_dir = "css"
-sass_dir = "_scss"
+sass_dir = "_sass"
 fonts_dir = "fonts"
 svg_dir = "svg"
 images_dir = "img"
